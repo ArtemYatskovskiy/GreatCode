292c386726455a159be06276426d5966fb417219
HoveR <artemadya2005@gmail.com>
2025-07-05 16:30:55 +0300

    First code

--- Main.js
+++ Main.js
@@ -1,1 +1,33 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+// function pisu(a, b){
+// a = "vanua"
+// b = "sex"
+// return a+b
+// }
+// console.log(pisu())
+
+// var ZP = 200
+// if(ZP<=200){
+//     ZP += 100
+// }
+// console.log(ZP)
+
+/*let cor = 12
+let vse = 347
+let pol
+let r
+
+pol = Math.floor(vse / cor)
+
+console.log(pol)
+
+r = vse % cor
+
+console.log(r)*/
+
+
+
+
+
+
